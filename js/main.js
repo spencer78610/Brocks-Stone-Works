8205709ec1807d1fdf53f09bd0e7c0bc8b1e3adb
spencer78610 <113371178+spencer78610@users.noreply.github.com>
2025-08-25 20:16:21 -0400

    made pages responsive

--- js/main.js
+++ js/main.js
@@ -19,3 +19,69 @@ document.addEventListener("DOMContentLoaded", () => {
     });
   });
 });
+
+//  ========== lightbox controls ==========
+
+document.addEventListener("DOMContentLoaded", () => {
+  const images = document.querySelectorAll(".gallery-img img");
+  const lightbox = document.getElementById("lightbox");
+  const lightboxImg = document.getElementById("lightbox-img");
+  const closeBtn = document.querySelector(".lightbox-close");
+  const prevBtn = document.querySelector(".lightbox-prev");
+  const nextBtn = document.querySelector(".lightbox-next");
+
+  let currentIndex = 0;
+
+  // Open Lightbox
+  function openLightbox(index) {
+    currentIndex = index;
+    lightbox.style.display = "flex";
+    lightboxImg.src = images[currentIndex].src;
+    lightbox.alt = images[currentIndex].alt;
+  }
+
+  // close lightbox
+  function closeLightbox() {
+    lightbox.style = "none";
+  }
+
+  // Show previous image
+  function showPrev() {
+    currentIndex = (currentIndex - 1 + images.length) % images.length;
+    lightboxImg.src = images[currentIndex].src;
+    lightboxImg.alt = images[currentIndex].alt;
+  }
+
+  // Show next image
+  function showNext() {
+    currentIndex = (currentIndex + 1) % images.length;
+    lightboxImg.src = images[currentIndex].src;
+    lightboxImg.alt = images[currentIndex].alt;
+  }
+
+  // add click event to all images
+  images.forEach((img, index) => {
+    img.addEventListener("click", () => openLightbox(index));
+  });
+
+  // Event listeners
+  closeBtn.addEventListener("click", closeLightbox);
+  prevBtn.addEventListener("click", showPrev);
+  nextBtn.addEventListener("click", showNext);
+
+  // close when clicking outside
+  lightbox.addEventListener("click", (e) => {
+    if (e.target === lightbox) {
+      closeLightbox();
+    }
+  });
+
+  // Keyboard navigation
+  document.addEventListener("keydown", (e) => {
+    if (lightbox.style.display = "flex") {
+      if (e.key === "ArrowLeft") showPrev();
+      if (e.key === "ArrowRight") showNext();
+      if (e.key === "Escape") closeLightbox();
+    }
+  });
+});
